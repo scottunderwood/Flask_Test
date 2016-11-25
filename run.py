7a80a3542962f38389bb102a486698c5029c32b6
scott.underwood87 <scott.underwood87@gmail.com>
2016-11-25 10:18:02 -0500

    completed flask lesson 3

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 from app import app
-#have augmented the run command to exclusively point to specific ip and port
-app.run(host="0.0.0.0", port=int("3000"), debug=True)
+#have augmented the run command to exclusively point to specific ip and port 
+app.run(host="0.0.0.0", port=int("8000"), debug=True)
